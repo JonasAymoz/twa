3147a5ae570d13f3c915a1a0eda6ef8abaff1ee4
JonasAymoz <jonas.aymoz@gmail.com>
2020-09-22 10:30:58 +0200

    Link typo

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,22 @@
-import Link from 'next/Link'
-import styles from '../styles/Header.module.css'
+import Link from "next/link";
+import styles from "../styles/Header.module.css";
 
 export default () => {
-    return <header className={styles.header}>
-          <Link href='/'><img src="/logoHeader.png" alt="logo" className={styles.logo}/></Link>
-          <div className={styles.headerLink}><Link href='/playlists'><h3 className="third"> + Playlists</h3></Link></div>        
-          <div className={styles.headerLink}><Link href='/contact'><h3 className="third">+ Contact</h3></Link></div>
-    </header>;
-  };
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <img src="/logoHeader.png" alt="logo" className={styles.logo} />
+      </Link>
+      <div className={styles.headerLink}>
+        <Link href="/playlists">
+          <h3 className="third"> + Playlists</h3>
+        </Link>
+      </div>
+      <div className={styles.headerLink}>
+        <Link href="/contact">
+          <h3 className="third">+ Contact</h3>
+        </Link>
+      </div>
+    </header>
+  );
+};
